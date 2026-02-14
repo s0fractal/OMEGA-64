914beee246a9821571d7cc55e4d91ebea2187daf
s0fractal <sergey.glova@gmail.com>
2026-02-14 20:35:54 +0200

    chore: canonize i16 clamp

--- i.L00.core.I16_CLAMP.ts
+++ i.L00.core.I16_CLAMP.ts
@@ -1,1 +1,5 @@
-export const I16_CLAMP = (x: number) => x > 32767 ? 32767 : (x < -32768 ? -32768 : x);
+import { I16_LIMITS } from "./i.L00.core.I16_LIMITS.ts";
+
+const I16 = I16_LIMITS();
+
+export const I16_CLAMP = (x: number) => x > I16.max ? I16.max : (x < I16.min ? I16.min : x);
