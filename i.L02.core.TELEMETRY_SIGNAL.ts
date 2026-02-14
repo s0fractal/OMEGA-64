baaea68c57ae72a0af6ae62ec4241191b17fa47d
s0fractal <sergey.glova@gmail.com>
2026-02-14 20:39:14 +0200

    feat: bridge telemetry to signal

---
+++ i.L02.core.TELEMETRY_SIGNAL.ts
@@ -0,0 +1,4 @@
+import { TELEMETRY } from "./i.L03.core.TELEMETRY.ts";
+import { SIGNAL } from "./i.L64.core.SIGNAL.ts";
+
+export const TELEMETRY_SIGNAL = (packet: ReturnType<typeof TELEMETRY>, type: "INFO" | "WARNING" | "REQUEST" | "ERROR" = "INFO") => SIGNAL.emit(type, { source: packet.source, message: packet.message, context: packet.timestamp ? { ...(packet.context ?? {}), timestamp: packet.timestamp } : packet.context });
